7cbcd3f65b6d04dd25c3dcdc4a942d3c97c1a637
Balamurugan <102686521+BalamuruganDV@users.noreply.github.com>
2022-04-12 18:16:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -437,7 +437,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [
             [
                 InlineKeyboardButton('💖ՏႮᏢᏢϴᎡͲ', url='https://t.me/MrJOKERgroup')
-                InlineKeyboardButton('ᏟᎻᎪΝΝᎬᏞ🎀', url='https://t.me/MrJOKERgroup')
+                InlineKeyboardButton('ᏟᎻᎪΝΝᎬᏞ', url='https://t.me/MrJOKERgroup')
             ],
             [
                 InlineKeyboardButton('🎬 ՏᎬᎡᏆᎬՏ ᎪΝᎠ ᎷϴᏙᏆᎬ ᏟᏞႮᏴ 🎬', url=f'https://t.me/mrJOKERCHANNEL')
